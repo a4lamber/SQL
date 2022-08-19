089eb09c52cc3c9cc4bb3ebb7853e1c827882132
Adam Zhang <adamhimy19@gmail.com>
2022-08-19 11:30:07 +0800

    did two order by practice

---
+++ SQL/SQL-cookbook-Practice/2-2-sorting-by-multiple-fields.sql
@@ -0,0 +1,15 @@
+/*
+Problem: sort rows from EMP table first by deptno ascending,
+then by salary descending.
+
+Discussion: 
+- Order of precedence in ORDERY BY is from left to right
+- it's okay to sort by columns not specified in select statement.
+But it's not true when you using GROUP BY or DISTINCE.
+- field is a term equivalent to column in SQL. (微信读书看到的)
+*/
+SELECT 
+    *
+FROM
+    emp
+order by deptno asc, sal desc
