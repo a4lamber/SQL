19984f993ac76e825c4cbbbf995337b830f3e3b4
TheZhang <a4lamber@users.noreply.github.com>
2023-08-29 10:51:24 -0400

    Update 1-1126-active-businesses.sql

--- SQL/SQL-LC/1-1126-active-businesses.sql
+++ SQL/SQL-LC/1-1126-active-businesses.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-
+-- Write your MySQL query statement below
+-- option 1:
 WITH CTE AS
 (
 SELECT
@@ -17,3 +17,28 @@ INNER JOIN CTE AS b ON a.event_type = b.event_type
 GROUP BY business_id
 HAVING COUNT(CASE WHEN occurences > average THEN 1 ELSE NULL END) > 1
 ORDER BY business_id ASC;
+
+-- option 2:
+-- 比option 1 看着更干净一些;
+-- 先确定average activity
+
+with cte as (
+    select
+        business_id,
+        event_type,
+        avg(occurences) as avg_occurences
+    from
+        events
+    group by event_type
+)
+select
+    e.business_id
+from
+    cte
+inner join events as e on 
+cte.business_id = e.business_id and 
+cte.event_type = e.event_type
+group by e.business_id
+-- more than 1 active business
+having count(case when occurences > avg_occurences then 1 else null end) > 1;
+
