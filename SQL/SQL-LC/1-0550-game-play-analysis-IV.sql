d72498c5397a00163e4641c4f2a4b76b513e43e6
TheZhang <a4lamber@users.noreply.github.com>
2023-08-29 07:45:23 -0400

    Update 1-0550-game-play-analysis-IV.sql
    update option 2 of 0550. beautify the join condition

--- SQL/SQL-LC/1-0550-game-play-analysis-IV.sql
+++ SQL/SQL-LC/1-0550-game-play-analysis-IV.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
+-- option 1:
+-- 
+-- 
 
 WITH CTE AS
 (
@@ -16,6 +18,22 @@ INNER JOIN CTE AS b ON (a.ranks+1 = b.ranks AND a.player_id = b.player_id)
 WHERE DATEDIFF(a.event_date,b.event_date) = -1 AND a.ranks = 1; 
 
 
+-- option 2:
+-- 基于第一种条件的优化，把where clause合并入join condition中去了;
+-- 分解需求
+-- features:
+-- players who logged in on the day after they first logged in --> some sort of sorting --> ranking
+-- 由于要求fraction, 分子或者分母很可能需要scalar subquery
+WITH CTE AS (
+SELECT
+    *, -- fix this later
+    rank() over(partition by player_id order by event_date) as log_in_rank
+FROM
+    Activity)
+SELECT
+    round(count(*)/(select count(distinct player_id) from Activity),2) AS fraction
+FROM
+    CTE AS a INNER JOIN CTE as b
+    ON a.player_id = b.player_id and a.log_in_rank + 1= b.log_in_rank and datediff(a.event_date,b.event_date) = -1
 
-
-# In MySQL: SELECT datediff('2016-03-01','2016-03-02') --> -1
+-- In MySQL: SELECT datediff('2016-03-01','2016-03-02') --> -1
