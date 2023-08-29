8ca532e8ed8b09814e3bca64f691b295af5508af
TheZhang <a4lamber@users.noreply.github.com>
2023-08-28 22:41:40 -0400

    Update 0-0197-rising-temperature.sql
    added left join method

--- SQL/SQL-LC/0-0197-rising-temperature.sql
+++ SQL/SQL-LC/0-0197-rising-temperature.sql
@@ -1,15 +1,24 @@
 -- Option 1:
-SELECT 
+-- SELECT 
+--     a.id
+-- FROM
+--     Weather a, Weather b
+-- WHERE DATEDIFF(a.recordDate,b.recordDate) = 1 AND a.temperature > 
+-- b.temperature;
+
+-- Option 2: 
+select
     a.id
-FROM
-    Weather a, Weather b
-WHERE DATEDIFF(a.recordDate,b.recordDate) = 1 AND a.temperature > 
-b.temperature;
+from
+    Weather as a
+LEFT JOIN weather as b
+ON datediff(a.recordDate,b.recordDate) = 1
+where a.temperature > b.temperature
 
 -- # The trick is to select a table twice. The 2nd one being the dummy
 -- # for filtering operation
 
--- # Option 2: not working when recordDate is not continuous
+-- # Option 3: not working when recordDate is not continuous
 -- # SELECT
 -- #     id
 -- # FROM
