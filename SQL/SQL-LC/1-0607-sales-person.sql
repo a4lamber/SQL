c4da474b25360c2d0a864c36d3deead159017482
TheZhang <a4lamber@users.noreply.github.com>
2023-08-29 09:31:09 -0400

    Update 1-0607-sales-person.sql
    simplifies it a little bit.

--- SQL/SQL-LC/1-0607-sales-person.sql
+++ SQL/SQL-LC/1-0607-sales-person.sql
@@ -1,3 +1,4 @@
+-- option 1
 SELECT
     name
 FROM
@@ -13,3 +14,20 @@ WHERE b.com_id = (SELECT com_id FROM Company WHERE name = "RED"));
 
 
 
+-- option 2
+-- 对于option 1的一种优化，毕竟where clause in option 1
+-- WHERE b.com_id = (SELECT com_id FROM Company WHERE name = "RED")) 和  where company.name = 'RED'
+-- 起到的效果是一样的
+select
+    name
+from
+    SalesPerson
+where sales_id not in (
+    select
+        sales_id
+    from
+        orders
+    inner join company 
+    on orders.com_id = company.com_id
+    where company.name = 'RED'
+)
